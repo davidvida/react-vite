ceca6760f5e3a25590f4253e1c3ae58e07225b6b
ximena subieta <ximegasub@gmail.com>
2023-04-05 14:53:12 -0400

    Added calendar in header

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { ItemProps } from "../../types/todo-item";
+import { AddText, Wrapper } from "./header.styles";
+import { TodoCalendar } from "../todo-calendar/todo-calendar.component"
 
 type Props = {
   dataLength: number;
 }
 
-const Header = ({dataLength}: Props) => <h3>Header ({dataLength})</h3>;
+const Header = ({dataLength}: Props) => <Wrapper><TodoCalendar/>
+<AddText>Today ({dataLength})</AddText></Wrapper>;
 
 export default Header;
 
---
+++ src/components/todo-calendar/calendar.styles.ts
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+
+
+export const AddCalendarMonth = styled.span`
+  position: absolute;
+  width: 15px;
+  height: 4px;
+  left: 20px;
+  top: 34px;
+  background: #3D82EB;
+  border: 1px solid #3D82EB;
+  border-radius: 2px 2px 0px 0px;
+  font-size: 4px;
+  color: #FFFFFF;
+  text-align: center;
+`
+
+export const AddCalendarDay = styled.div`
+  box-sizing: border-box;
+  position: absolute;
+  width: 17px;
+  height: 11px;
+  left: 20px;
+  top: 39px;
+  border: 1px solid #3D82EB;
+  border-radius: 0px 0px 3px 3px;
+  font-size: 7px;
+  color: #3D82EB;
+  text-align: center;
+`
