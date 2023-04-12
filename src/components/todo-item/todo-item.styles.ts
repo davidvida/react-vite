c2beafbccbb3b082478b46b7adde49a1a86d2e86
Carlos Zurita <czuritarojas@gmail.com>
2023-04-12 01:28:57 -0400

    Add styles to the remove button component

--- src/components/todo-item/todo-item.styles.ts
+++ src/components/todo-item/todo-item.styles.ts
@@ -1,8 +1,10 @@
 import styled from "@emotion/styled";
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 export const Wrapper = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 8px 10px;
   &:hover {
     box-shadow: 0px 3px 6px 1px rgba(0, 0, 0, 0.15);
@@ -12,4 +14,10 @@ export const Wrapper = styled.div`
 
 export const TodoDescription = styled.div`
   font-size: 10px;
+  align-self: center;
 `
+export const RemoveIcon = styled(HighlightOffIcon)`
+  color: #EB3D3D;
+  width: 12px;
+  height: 12px;
+`;
--- src/components/todo-item/todo.item.component.tsx
+++ src/components/todo-item/todo.item.component.tsx
@@ -1,10 +1,10 @@
 import React, { useContext, useState } from "react";
 import { ItemProps, ItemStatus } from "../../types/todo-item";
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import TodoItemStatus from "../todo-item-status/todo-item-status.component";
-import { TodoDescription, Wrapper } from "./todo-item.styles";
+import { RemoveIcon, TodoDescription, Wrapper } from "./todo-item.styles";
 import { AppContext } from "../../context/app-context";
 import { Types } from "../../reducer/actions";
+import { Stack } from "@mui/material";
 
 type Props = {
   item: ItemProps;
@@ -23,22 +23,22 @@ const TodoItem = ({ item }: Props) => {
 
   return (
     <Wrapper>
-      <TodoItemStatus status={status} id={id}/>
-      <TodoDescription
-        style={{ textDecoration: status === ItemStatus.DONE ? 'line-through' : '' }}
-        onClick={() => dispatch({ 
-          type: Types.Select,
-          payload: {
-              id: id
-            } 
-          })
-        }
-      >
-        {description}
-      </TodoDescription>
-      <span>
-        <button onClick={() => handleRemoveItem(item.id)}>x</button>
-      </span>
+      <Stack direction='row'>
+        <TodoItemStatus status={status} id={id}/>
+        <TodoDescription
+          style={{ textDecoration: status === ItemStatus.DONE ? 'line-through' : '' }}
+          onClick={() => dispatch({ 
+            type: Types.Select,
+            payload: {
+                id: id
+              } 
+            })
+          }
+        >
+          {description}
+        </TodoDescription>
+      </Stack>
+      <RemoveIcon onClick={() => handleRemoveItem(item.id)}>x</RemoveIcon>
     </Wrapper>
 );
 }
