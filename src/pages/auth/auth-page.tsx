16788f932d5091c9dc9c9171c48c2f3dc8080979
Bernarth <alberthbernarth75@gmail.com>
2023-04-27 22:16:40 -0400

    Add routes for auth

---
+++ src/pages/auth/auth-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Outlet } from "react-router-dom";
+
+const AuthPage = () =>
+  <div>
+    <Outlet />
+  </div>;
+
+export default AuthPage;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -9,13 +9,17 @@
     /auth/resetPassword => AuthResetPasswordPage
 */
 import React, { Suspense } from "react";
-import { Outlet, Routes, Route } from "react-router-dom";
+import { Outlet, Routes, Route, Navigate } from "react-router-dom";
 import LoadingPageIndicator from "@/components/loading-page-indicator/loading-page-indicator.component";
 
 const HomePage = React.lazy(() => import('@/pages/home/home-page'));
 const TodosPage = React.lazy(() => import('@/pages/todos/todos-page'));
 const ConfigPage = React.lazy(() => import('@/pages/config/config-page'));
 const NotFoundPage = React.lazy(() => import('@/pages/not-found/not-found-page'));
+const AuthPage = React.lazy(() => import('@/pages/auth/auth-page'));
+const LoginPage = React.lazy(() => import('@/pages/auth/login/login-page'));
+const RegisterPage = React.lazy(() => import('@/pages/auth/register/register-page'));
+const ResetPasswordPage = React.lazy(() => import('@/pages/auth/reset-password/reset-password-page'));
 
 const AppRoutes = () => {
   return (
@@ -42,6 +46,29 @@ const AppRoutes = () => {
           </Suspense>
           }
         />
+        {/* It doesn't work when refreshing the page */}
+        <Route path="/auth/*" element={<AuthPage />}> {/* It works without the AuthPage */}
+          <Route index element={<Navigate to="login" />} />
+          <Route path="login" element={
+              <Suspense fallback={<LoadingPageIndicator />}>
+                <LoginPage />
+              </Suspense>
+            }
+          />
+          <Route path="register" element={
+              <Suspense fallback={<LoadingPageIndicator />}>
+                <RegisterPage />
+              </Suspense>
+            }
+          />
+          <Route path="resetPassword" element={
+              <Suspense fallback={<LoadingPageIndicator />}>
+                <ResetPasswordPage />
+              </Suspense>
+            }
+          />
+        </Route>
+
       </Route>
       <Route path="*" element={
         <Suspense fallback={<LoadingPageIndicator />}>
